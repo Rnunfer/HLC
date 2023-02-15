c44fb5ecd23fb44e52e001d3a6b114dc7bfaaf2f
Jchabol1206 <juachabol@gmail.com>
2023-02-15 09:52:59 +0100

    rectangulo circulo y triangulo hecho

--- src/app/poligonos/triangulo/triangulo.component.ts
+++ src/app/poligonos/triangulo/triangulo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PoligonoService } from '../../poligono.service';
 
 @Component({
   selector: 'app-triangulo',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   ]
 })
 export class TrianguloComponent {
+  perimetro : number;
+  area:number;
 
+  constructor(private poligonoService:PoligonoService){
+    this.perimetro= this.calcPerimetro(poligonoService.getLado());
+    this.area= this.calcArea(poligonoService.getLado());
+  }
+
+  calcPerimetro(lado:number) : number{
+    return lado*3;
+  }
+
+
+  calcArea(lado:number) : number{
+    return Math.sqrt(3/4)*Math.pow(lado, 2);
+  }
 }
